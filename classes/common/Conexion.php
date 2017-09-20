4b88be7effb06df00961e886526f90c4d4634462
Luis Enrique Condori Abrigo <luisabrigo@hotmail.com>
2017-09-20 09:46:45 -0500

    revisando archivo conexion y constantes. Ademas se crea la vista productos-listar

--- classes/common/Conexion.php
+++ classes/common/Conexion.php
@@ -1,20 +1,21 @@
 <?php
+//creando calse conexion
 class Conexion {
-    
-    private static $instance = NULL;
+    //singleton - asegura que solo usemos un objeto creado y no estar creado a cada momento
+    private static $instance = NULL; // lo retornado verifica el objetocon los datos obtenidos
     
     private function __construct() {
         
     }
-
+	//si no tiene valor crea un objeto pdo a partir de las constantes (ver constantes.php) para armar la conexion
     public static function getConexion() {
         if(self::$instance == NULL){
             $host = Constantes::DB_HOST;
             $dbname = Constantes::DB_SCHEMA;
             self::$instance = new PDO("mysql:host=$host;dbname=$dbname", Constantes::DB_USERNAME, Constantes::DB_PASSWORD);
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //correcion y manjeo de errores
         }
-        return self::$instance;
+        return self::$instance; //devuelve le objeto pdo
     }
     
 }
---
+++ productos-listar.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>storelocal</title>
+       
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+		
+		<!--bootstrap css-->
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>             
+        
+        <!--bootstrap js mas jquery-->
+        <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        
+		<!--contenedor-->
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <!-- logo -->
+                <div class="navbar-header">
+                    <!-- boton para el menu desplegable -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-menu" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img alt="Brand" src="img/logo.png" height="24">
+                      </a>
+                </div>
+                
+                <!-- menu de opciones -->
+                <div class="collapse navbar-collapse" id="top-menu">
+                    
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="contactos.php">Contacto</a></li>
+                        
+                        <!-- dropdown -->
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mantenimiento <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="roles-listar.php">Roles</a></li>
+                                <li><a href="usuarios-listar.php">Usuarios</a></li>
+                                <li><a href="categorias-listar.php">Categorias</a></li>
+                                <li><a href="productos-listar.php">Productos</a></li>
+                                <li><a href="clientes-listar.php">Clientes</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Configuración</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reportes <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="productos-reporte.php">Reporte de Stock</a></li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                    
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Perfil</a></li>
+                        <li><a href="logout.php">Salir</a></li>
+                    </ul>
+                    <p class="navbar-text navbar-right">Bienvenido</p>
+                    
+                </div>
+                
+            </div>
+        </nav>
+		<!--navegador-->
+        
+		<!--contenedor-->
+        <div class="container-fluid">
+            
+            <div class="panel panel-default">
+                
+                <div class="panel-heading">
+                    <h3 class="panel-title">Listado de Productos</h3>
+                </div>
+            
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>CATEGORÍA</th>
+                            <th>MODELO</th>
+                            <th>PRECIO</th>
+                            <th>IMAGEN</th>
+                            <th>ESTADO</th>
+                            <th width="50"></th>
+                            <th width="50"></th>
+                            <th width="50"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                   	 		
+                    </tbody>
+                </table>
+            
+                <div class="panel-footer">
+                    <a href="productos-nuevo.php" class="btn btn-primary">Nuevo</a>
+                </div>
+                
+            </div>
+                
+        </div>
+		<!--contenedor-->
+        
+		<!--pie de pagina-->
+        <footer class="footer">
+            <div class="container-fluid">
+                <p class="text-muted text-center">Lima, Tecsup.</p>
+            </div>
+        </footer>
+        <!--pie de pagina-->
+    </body>
+</html>
