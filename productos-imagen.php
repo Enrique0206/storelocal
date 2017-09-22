36385362078acb3afe48103c600c0393e2b6e141
Luis Enrique Condori Abrigo <luisabrigo@hotmail.com>
2017-09-22 09:50:59 -0500

    ingresasndo codigo  a productos imagen

--- productos-imagen.php
+++ productos-imagen.php
@@ -1,11 +1,19 @@
 <?php
-
 require_once './autoload.php';
-
-try{
+try {
     
-        
-} catch (Exception $ex) {
-    die('Error: '  . $ex->getMessage());
-}
+    //$id = $_GET['id'];//obtener el id seleccionado
+    $id = 10;
+    $producto = ProductosDAO::obtener($id);
+    //este file va aestar constituido por la constante que tiene la ruta y se aplicar el metodo y funcion sobre imagen
+    $filename = Constantes::RUTA_IMAGENES . $producto->imagen;
+    
+    header("Content-type: ".$producto->imagen_tipo);
+    header("Content-Length: ".$producto->imagen_tamanio); 
+    header("Content-Disposition: inline; filename=".$producto->imagen); //'Content-Disposition: attachment' si se quiere forzar la descarga
 
+    echo file_get_contents($filename);
+    
+} catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+}
